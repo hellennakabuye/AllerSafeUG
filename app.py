1be4e4f52af5fae4a7a92fb8d3bca1f93eff965e
hellennakabuye <55150746+hellennakabuye@users.noreply.github.com>
2026-02-03 11:24:20 +0300

    Remove user input saving for flare probability
    Removed the user input saving functionality from the flare probability section.

--- app.py
+++ app.py
@@ -170,13 +170,6 @@ with col1:
     if st.session_state.flare_prob is not None:
         prob = st.session_state.flare_prob
 
-        save_user_input(
-            season, temperature, humidity,
-            dust_level, smoke_level,
-            outdoor_time, medication_used,
-            symptom_score, prob
-        )
-
         st.metric(
             label="Flare-Up Probability",
             value=f"{prob * 100:.1f}%"
